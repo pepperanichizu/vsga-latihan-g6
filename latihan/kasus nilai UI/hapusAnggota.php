ba833a75a6a7bba2cf23dcca5bfda993ff958174
N Rani S <nahdia.rani.sholihah@mail.ugm.ac.id>
2023-06-19 02:09:11 +0700

    fix delete method

--- latihan/kasus nilai UI/hapusAnggota.php
+++ latihan/kasus nilai UI/hapusAnggota.php
@@ -1,5 +1,5 @@
 <?php include 'koneksi.php';
-    $idAnggota = $_GET['idAnggota'];
+    $idAnggota = $_GET['id_anggota'];
 
     $query = mysqli_query($koneksi, "DELETE FROM anggota WHERE id_anggota='$idAnggota'");
 
